58b74057f641f4488364b9bfe4d1b46b937a7c61
luan048 <luanmonteiropedrosa@gmail.com>
2024-09-20 23:32:08 -0300

    Initial Archives

---
+++ Localizacao/script.js
@@ -0,0 +1,16 @@
+navigator.geolocation.getCurrentPosition(position => {
+    const {latitude, longitude} = position.coords
+
+    let map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([longitude, latitude]),
+            zoom: 10
+        })
+    })
+})
